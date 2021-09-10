cb570952eac22c411223eb189e1576e8a3d89112
Swaggero_o <mugel849@gmail.com>
2021-09-10 19:28:42 +0200

    responsive buttons

--- scripts/votingPage.js
+++ scripts/votingPage.js
@@ -3,7 +3,11 @@ function best(picID, projectID){
     let oReq = new XMLHttpRequest();
     let parms = "picID="+picID+"&projectID="+projectID;
     oReq.addEventListener("load",function() {
-        document.getElementById("best"+picID).classList.add("bested");
+        if(!document.getElementById("best"+picID).classList.contains("bested")){
+            document.getElementById("best"+picID).classList.add("bested");
+        }else {
+            document.getElementById("best"+picID).classList.remove("bested");
+        }
     })
     oReq.open("POST","../php/bestManager.php");
     oReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
@@ -15,7 +19,11 @@ function like(picID){
     let oReq = new XMLHttpRequest();
     let parms = "picID="+picID;
     oReq.addEventListener("load",function() {
-        document.getElementById("like"+picID).classList.add("liked");
+        if (!document.getElementById("like"+picID).classList.contains("liked")){
+            document.getElementById("like"+picID).classList.add("liked");
+        }else{
+            document.getElementById("like"+picID).classList.remove("liked");
+        }
     })
     oReq.open("POST","../php/likeManager.php");
     oReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
