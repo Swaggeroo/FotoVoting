9c37d2b83f9c37084842ec54316475aff2f92ac6
Swaggeroo <mugel849@gmail.com>
2021-09-14 21:47:27 +0200

    delete button for own pictures bugfixes

--- php/deleteManager.php
+++ php/deleteManager.php
@@ -3,13 +3,10 @@ if(!isset($_SESSION)){
     session_start();
 }
 
-require "../php/dbConnection.php";
-
+require "./dbConnection.php";
 $db = new db();
 
 if(intval($_SESSION['userAccountLevel']) == 2 || intval($_SESSION['userID']) == intval($db->getUserIDFromPicID(intval($_POST['picID'])))){
-    require "./dbConnection.php";
-    $db = new db();
     $db->deletePicture(intval($_POST['picID']));
 }
 ?>
