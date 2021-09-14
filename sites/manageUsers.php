c41f83eb130950dc5110f71211d10826d1ee3790
Swaggero_o <mugel849@gmail.com>
2021-09-14 11:12:11 +0200

    web app

--- sites/manageUsers.php
+++ sites/manageUsers.php
@@ -48,7 +48,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="../media/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="../media/favicon/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="../media/favicon/favicon-16x16.png">
-    <link rel="manifest" href="../media/favicon/manifest.json">
+    <link rel="manifest" href="../media/favicon/manifest.webmanifest">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
